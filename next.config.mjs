16f86a844e754de2763372356be0466d9f779da8
Houss82 <houssem1982mansour@gmail.com>
2025-12-18 07:57:06 +0100

    correction page 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/blog-0',
+        destination: '/blog',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/blog-1',
+        destination: '/blog',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
